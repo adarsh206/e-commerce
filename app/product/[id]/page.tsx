7df17c29bf4a8475b9d44560f82894360ab2253e
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-07-08 13:57:11 +0530

    complete project

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -7,7 +7,7 @@ import { useSupabase } from '@/lib/supabase/hooks/useSupabase';
 import { useParams } from 'next/navigation'
 import React, { useEffect } from 'react'
 
-const page = () => {
+const ProductPage  = () => {
     const { id }  = useParams();
     const { singleProduct, getSingleProduct } = useSupabase();
 
@@ -23,4 +23,4 @@ const page = () => {
   )
 }
 
-export default page
+export default ProductPage 
